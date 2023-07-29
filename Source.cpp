7549886afa29a846a3260018b1e95c5936d05423
rxm3n <41157757+RayyuShao@users.noreply.github.com>
2023-07-28 20:44:46 -0400

    Created main function Includes a try catch block with 3 catches 5 test cases taken from the sample output, uncomment the error cases to see error output.

--- Source.cpp
+++ Source.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 using namespace std;
 
-// Custom exception classes
+// custom exception structs
 struct InvalidCharacterException : public exception {
     const char* what() const noexcept override {
         return "Invalid character. Only A-Z and a-z are allowed.";
@@ -18,29 +18,29 @@ struct InvalidRangeException : public exception {
     }
 };
 
-// Function to calculate character offsets
+// function to calculate character offsets
 char character(char start, int offset) {
-    // Check if the start character is a letter (A-Z or a-z)
+    // check if the start character is a letter (A-Z or a-z)
     if (!((start >= 'A' && start <= 'Z') || (start >= 'a' && start <= 'z'))) {
         throw InvalidCharacterException();
     }
 
-    // Check if the offset is within the range of A-Z and a-z
+    // check if the offset is within the range of A-Z and a-z
     if ((start + offset < 'A' || start + offset > 'Z') && (start + offset < 'a' || start + offset > 'z')) {
         throw InvalidRangeException();
     }
 
-    // Calculate and return the character after applying the offset
+    // calculate and return the character after applying the offset
     return start + offset;
 }
 
 int main() {
     try {
         cout << "character('a', 1) returns: " << character('a', 1) << endl;
-        cout << "character('a', -1) should throw an InvalidRangeException." << endl;
+        cout << "character('a', -1) should throw an InvalidRangeException." /* << character('a', -1)*/ << endl; //uncomment to see error
         cout << "character('Z', -1) returns: " << character('Z', -1) << endl;
-        cout << "character('?', 5) should throw an InvalidCharacterException." << character('?', 5) << endl;
-        cout << "character('A', 32) should throw an exception." << character('A', 32) << endl;
+        cout << "character('?', 5) should throw an InvalidCharacterException." /* << character('?', 5) */ << endl; //uncomment to see error
+        cout << "character('A', 32) should throw an exception." /*<< character('A', 32)*/ << endl; //uncomment to see error
     }
     catch (const InvalidCharacterException& e) {
         cerr << "Exception caught: " << e.what() << endl;
